4528c20f0e586fc092424072528636f12a45c7c9
Aleksejs Fomins <aleksejs.fomins@uzh.ch>
2021-03-22 23:43:42 +0100

    minor

--- mesostat/utils/signals/fit.py
+++ mesostat/utils/signals/fit.py
@@ -11,6 +11,21 @@ def polyfit_transform(x, y, ord=1):
 
 
 def natural_cubic_spline_fit_reg(x, y, dof=5, alpha=0.01):
-    basis = cr(x, df=dof, constraints="center")
-    model = Ridge(alpha=alpha).fit(basis, y)
-    return model.predict(basis)
+    if not np.any(np.isnan([x, y])):
+        basis = cr(x, df=dof, constraints="center")
+        model = Ridge(alpha=alpha).fit(basis, y)
+        return model.predict(basis)
+    else:
+        # Insurance against NAN
+        # Solution: Fit only non-nan pairs, leave rest as NAN
+
+        print("Warning, have NAN")
+
+        iNanX = np.isnan(x)
+        iNanY = np.isnan(y)
+        iNan = np.logical_or(iNanX, iNanY)
+
+        yRez = np.copy(y)
+        yRez[iNan] = np.nan
+        yRez[~iNan] = natural_cubic_spline_fit_reg(x[~iNan], y[~iNan], dof=dof, alpha=alpha)
+        return yRez
