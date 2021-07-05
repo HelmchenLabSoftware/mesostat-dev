a418eca5fc23f53064f42c7f431e83f88a02355f
Aleksejs Fomins <aleksejs.fomins@uzh.ch>
2021-07-05 18:00:13 +0200

    pid4D

--- mesostat/metric/idtxl_pid.py
+++ mesostat/metric/idtxl_pid.py
@@ -6,15 +6,59 @@ from idtxl.data import Data
 from mesostat.utils.decorators import redirect_stdout
 
 
-def _parse_channels(settings):
+def _parse_channels(settings, dim):
     if 'channels' in settings.keys():
-        assert len(settings['channels']) == 3
-        src = settings['channels'][:2]
-        trg = settings['channels'][2]
+        assert len(settings['channels']) == dim
+        src = settings['channels'][:-1]
+        trg = settings['channels'][-1]
     else:
         src = settings['src']
         trg = settings['trg']
     return src, int(trg)
+
+
+def bivariate_pid_key():
+    return [
+        'unq_s1',
+        'unq_s2',
+        'shd_s1_s2',
+        'syn_s1_s2'
+    ]
+
+
+# Stacking output into numpy array for efficiency
+# Making sure that output always has the same order
+def multivariate_pid_key(dim):
+    if dim == 3:
+        return [
+            ((1,),),            # Unq1
+            ((2,),),            # Unq2
+            ((1,), (2,),),      # Shd12
+            ((1, 2,),)          # Syn12
+        ]
+    elif dim == 4:
+        return [
+            ((1,),),                    # Unq_1
+            ((2,),),                    # Unq_2
+            ((3,),),                    # Unq_3
+            ((1,), (2,)),               # Shd_12
+            ((1,), (3,)),               # Shd_13
+            ((2,), (3,)),               # Shd_23
+            ((1,), (2,), (3,)),         # Shd_123
+            ((1, 2),),                  # Syn_12
+            ((1, 3),),                  # Syn_13
+            ((2, 3),),                  # Syn_23
+            ((1, 2, 3),),               # Syn_123
+            ((1,), (2, 3)),             # {1}{23}
+            ((2,), (1, 3)),             # {2}{13}
+            ((3,), (1, 2)),             # {3}{12}
+            ((1, 2), (1, 3)),           # {12}{13}
+            ((1, 2), (2, 3)),           # {12}{23}
+            ((1, 3), (2, 3)),           # {13}{23}
+            ((1, 2), (1, 3), (2, 3))    # {12}{13}{23}
+        ]
+    else:
+        raise ValueError('Unexpected dimension', dim)
 
 
 @redirect_stdout
@@ -22,15 +66,10 @@ def bivariate_pid_3D(data, settings):
     dataIDTxl = Data(data, dim_order='rps', normalise=False)
     pid = BivariatePID()
 
-    src, trg = _parse_channels(settings)
+    src, trg = _parse_channels(settings, dim=3)
     rez = pid.analyse_single_target(settings=settings['settings_estimator'], data=dataIDTxl, target=trg, sources=src)
 
-    return np.array([
-        rez.get_single_target(trg)['unq_s1'],
-        rez.get_single_target(trg)['unq_s2'],
-        rez.get_single_target(trg)['shd_s1_s2'],
-        rez.get_single_target(trg)['syn_s1_s2']
-    ])
+    return np.array([rez.get_single_target(trg)[k] for k in bivariate_pid_key()])
 
 
 @redirect_stdout
@@ -38,12 +77,18 @@ def multivariate_pid_3D(data, settings):
     dataIDTxl = Data(data, dim_order='rps')
     pid = MultivariatePID()
 
-    src, trg = _parse_channels(settings)
+    src, trg = _parse_channels(settings, dim=3)
     rez = pid.analyse_single_target(settings=settings['settings_estimator'], data=dataIDTxl, target=trg, sources=src)
 
-    return np.array([
-        rez.get_single_target(trg)['avg'][((1,),)][2],
-        rez.get_single_target(trg)['avg'][((2,),)][2],
-        rez.get_single_target(trg)['avg'][((1,), (2,),)][2],
-        rez.get_single_target(trg)['avg'][((1, 2,),)][2]
-    ])
+    return np.array([rez.get_single_target(trg)[k] for k in multivariate_pid_key(dim=3)])
+
+
+@redirect_stdout
+def multivariate_pid_4D(data, settings):
+    dataIDTxl = Data(data, dim_order='rps')
+    pid = MultivariatePID()
+
+    src, trg = _parse_channels(settings, dim=4)
+    rez = pid.analyse_single_target(settings=settings['settings_estimator'], data=dataIDTxl, target=trg, sources=src)
+
+    return np.array([rez.get_single_target(trg)[k] for k in multivariate_pid_key(dim=4)])
--- mesostat/utils/iterators/matrix.py
+++ mesostat/utils/iterators/matrix.py
@@ -1,27 +1,35 @@
 
+
 # Return a list of all pairs of elements in a list, excluding flips.
-def iter_g_2D(lst):
-    n = len(lst)
+def iter_g_2D(n):
     assert n >= 2
     for i in range(n):
         for j in range(i + 1, n):
-            yield [lst[i], lst[j]]
+            yield i, j
 
 
-def iter_gg_3D(lst):
-    n = len(lst)
+def iter_gg_3D(n):
     assert n >= 3
     for i in range(n):
         for j in range(i + 1, n):
             for k in range(j + 1, n):
-                yield [lst[i], lst[j], lst[k]]
+                yield i, j, k
 
 
-def iter_gn_3D(lst):
-    n = len(lst)
+def iter_gn_3D(n):
     assert n >= 3
     for i in range(n):
         for j in range(i + 1, n):
             for k in range(n):
                 if (k != i) and (k != j):
-                    yield [lst[i], lst[j], lst[k]]
+                    yield i, j, k
+
+
+def iter_ggn_4D(n):
+    assert n >= 4
+    for i in range(n):
+        for j in range(i + 1, n):
+            for k in range(j + 1, n):
+                for l in range(n):
+                    if (l != i) and (l != j) and (l != k):
+                        yield i, j, k, l
